5813e33aa492995fbf2ca7eee344f0eba7d6c041
chrischenlixing <chrischenlixing@gmail.com>
2024-01-23 05:40:34 -0800

    done-not-needuncomment

---
+++ notification-service/src/handlers/sendMail.js
@@ -0,0 +1,40 @@
+import AWS from "aws-sdk";
+
+const ses = new AWS.SES({ region: 'eu-west-1' });
+async function sendMail(event, context) {
+    const record = event.Records[0];
+    console.log('record processing', record);
+
+    const email = JSON.parse(record.body);
+    const { subject, body, recipient } = email;
+
+    const params = {
+        Source: 'chrischenlixing@gmail.com',
+        Destination: {
+            ToAddresses: [recipient],
+        },
+        Message: {
+            Body: {
+                Text: {
+                    Data: body,
+                },
+            },
+            Subject: {
+                Data: subject,
+            },
+        },
+    };
+    try{
+        const result = await ses.sendEmail(params).promise();
+        console.log(result);
+        return result;
+    }
+    catch(error){
+        console.error(error);
+    }
+
+}
+
+export const handler = sendMail;
+
+
